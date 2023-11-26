a1c9ccbb20755ec38c21477270e2044dab0ca4dc
adi <aadhivenkat4@gmail.com>
2023-11-26 19:51:45 +0530

    migrated to stanalone

--- my-portfolio/src/app/about/about.component.spec.ts
+++ my-portfolio/src/app/about/about.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AcboutComponent } from './acbout.component';
+import { AboutComponent } from './about.component';
 
-describe('AcboutComponent', () => {
-  let component: AcboutComponent;
-  let fixture: ComponentFixture<AcboutComponent>;
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AcboutComponent]
+      declarations: [AboutComponent]
     });
-    fixture = TestBed.createComponent(AcboutComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -4,17 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcboutComponent } from './acbout/acbout.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AcboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, AboutComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AcboutComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
