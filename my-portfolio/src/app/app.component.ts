5d81b6f44ba87eb4a0a1f26330cc41efe1037289
adi <aadhivenkat4@gmail.com>
2023-11-26 21:39:41 +0530

    material added

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports:[AboutComponent, MatCardModule]
 })
 export class AppComponent {
   title = 'my-portfolio';
