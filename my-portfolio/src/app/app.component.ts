6ef1e5206a7e485600a6ba082ae672d767a99587
adi <aadhivenkat4@gmail.com>
2024-03-03 21:44:13 +0530

    base component changes done

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from './about/about.component';
 import {MatCardModule} from '@angular/material/card';
+import { HeaderComponent } from './header/header.component';
+import { BaseComponent } from './base/base.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports:[AboutComponent, MatCardModule]
+  imports:[AboutComponent, MatCardModule, HeaderComponent, BaseComponent]
 })
 export class AppComponent {
   title = 'my-portfolio';
