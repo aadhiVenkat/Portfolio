4bf71b7c112b642ad38620f96aad1cdd61ab4775
adi <aadhivenkat4@gmail.com>
2024-03-05 00:21:10 +0530

    about component changes

--- my-portfolio/src/app/about/about.component.ts
+++ my-portfolio/src/app/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  imports: [MatCardModule]
 })
 export class AboutComponent {
 
