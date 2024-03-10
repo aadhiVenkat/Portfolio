8553dd8b851e1e8fe49412e067d7ec059c62e1a8
adi <aadhivenkat4@gmail.com>
2024-03-10 21:36:49 +0530

    scroll to target added

--- my-portfolio/src/app/header/header.component.ts
+++ my-portfolio/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -12,5 +12,38 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @ViewChild("base", {static: false}) base! : ElementRef;
+  @ViewChild("about", {static: false}) about! : ElementRef;
+  @ViewChild("skills", {static: false}) skills! : ElementRef;
+  @ViewChild("proficiencies", {static: false}) proficiencies! : ElementRef;
+  @ViewChild("contact", {static: false}) contact! : ElementRef;
 
+  constructor(private eleRef:ElementRef){}
+
+  scrollToTarget(target:string) {
+  //  var headDiv = this.eleRef.nativeElement.querySelector('.toolbar').offsetHeight;
+  //  var targetDiv = this.base?.nativeElement.offsetTop - headDiv;
+  //  debugger
+  //  switch(target){
+  //   case "base":
+  //     targetDiv = this.base?.nativeElement.offsetTop - headDiv;
+  //     break;
+  //   case "about":
+  //     targetDiv = this.about?.nativeElement.offsetTop - headDiv;
+  //     break;
+  //   case "skills":
+  //     targetDiv = this.skills?.nativeElement.offsetTop - headDiv;
+  //     break;
+  //   case "proficiencies":
+  //     targetDiv = this.proficiencies?.nativeElement.offsetTop - headDiv;
+  //     break;
+  //   case "contact":
+  //     targetDiv = this.contact?.nativeElement.offsetTop - headDiv;
+  //     break;
+  //  }
+   const targetElement = document.getElementById(target);
+   targetElement?.scrollIntoView({behavior:'smooth', block:'start'})
+  //  window.scrollTo({top:targetDiv, behavior:'smooth'});
+}
+ 
 }
