9ae8984e08cb312d6fa2352431b67c5811f8e45d
adi <aadhivenkat4@gmail.com>
2024-03-17 11:31:38 +0530

    contact excel details added

--- my-portfolio/src/app/contact/contact.component.ts
+++ my-portfolio/src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -14,11 +15,22 @@ import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angu
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  
   email = new FormControl('', [Validators.required, Validators.email]);
   companyName = new FormControl('', [Validators.required]);
   message = new FormControl('', [Validators.required]);
+  disbaleSubmit:boolean = true;
 
+  ngOnInit() {
+    merge(
+      this.email.valueChanges,
+      this.companyName.valueChanges,
+      this.message.valueChanges,
+      ).subscribe(()=>{
+        this.disbaleSubmit = !(this.email.valid && this.companyName.valid && this.message.valid);
+    })
+  }
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
@@ -26,5 +38,55 @@ export class ContactComponent {
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+
+  submit(){
+    const formData = new FormData();
+    const email_value:string = this.email.value ? this.email.value : "";
+    const companyName_value:string = this.companyName.value ? this.companyName.value : "";
+    const message_value:string = this.message.value ? this.message.value : "";
+    formData.append("email",email_value);
+    formData.append("companyName",companyName_value);
+    formData.append("message",message_value);
+    // fetch("https://getform.io/f/zaxmdegb", {
+    //     method: "POST",
+    //     body: formData,
+    //     headers: {
+    //         "Accept": "application/json",
+    //     },
+    // })
+    // .then(response => {
+    //     if (!response.ok) {
+    //         throw new Error(`An error occurred: ${response.statusText}`);
+    //     }else{
+    //       this.email.reset();
+    //       this.companyName.reset();
+    //       this.message.reset();
+    //     }
+    //     console.log(response);
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    // });
+
+    const url = "https://script.google.com/macros/s/AKfycbyhbSqnm-e-tsM2gtVQd3cp29SUosjFFODzbbAazKRhRmz_xCzWIgmxKbnPvW9Z7BxaOQ/exec"
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`An error occurred: ${response.statusText}`);
+        }else{
+          this.email.reset();
+          this.companyName.reset();
+          this.message.reset();
+        }
+        console.log(response);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+  }
+
 
 }
