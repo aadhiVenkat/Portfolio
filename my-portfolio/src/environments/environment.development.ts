2096cb49f7911ab65dfa08979a9a8ffa1b77b8ce
adi <aadhivenkat4@gmail.com>
2024-03-17 13:42:11 +0530

    fire base added ci/cd

---
+++ my-portfolio/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+     // npm run ng generate environments  to generate environments
+     production: false,
+     firebase: {
+         apiKey: "AIzaSyAEYLul3q7O6R27eOuICOHzEvW5v7PZsuo",
+         authDomain: "aadhivenkat-1379c.firebaseapp.com",
+         projectId: "aadhivenkat-1379c",
+         storageBucket: "aadhivenkat-1379c.appspot.com",
+         messagingSenderId: "1009295441571",
+         appId: "1:1009295441571:web:09cd1b0df76adb8a604916",
+         measurementId: "G-4J24H4YSYK"
+     }
+};
--- my-portfolio/src/main.ts
+++ my-portfolio/src/main.ts
@@ -1,8 +1,15 @@
-import { importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {AngularFireModule} from '@angular/fire/compat'
+import { environment } from './environments/environment.development';
+import { initializeApp } from 'firebase/app';
+if (environment.production) {
+  enableProdMode();
+}
 
+const firebaseApp = initializeApp(environment.firebase);
 
 bootstrapApplication(AppComponent, {
     providers: [provideAnimations()]
